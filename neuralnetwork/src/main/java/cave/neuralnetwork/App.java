5e5d51a62ac61ac38f485b82ad54a5c67f0293bc
John Purcell <john@caveofprogramming.com>
2021-10-16 14:30:47 +0200

    01110 Perceptron AND

--- neuralnetwork/src/main/java/cave/neuralnetwork/App.java
+++ neuralnetwork/src/main/java/cave/neuralnetwork/App.java
@@ -1,40 +1,43 @@
 package cave.neuralnetwork;
 
-
+// @formatter:off
 /*
+ *
  * INPUT	AND		OR	XOR		NOR		NAND  	XNOR
  * 00		0		0	0		1		1	  	1
  * 01		0		1	1		0		1	 	0
  * 10		0		1	1		0		1		0
  * 11		1		1	0		0		0		1
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-    	// inputs
-        double[] x = {0, 1};
-        
-        // weights
-        double[] w = {0.5, 0.5};
-        
-        // bias
-        double b = 0.5;
-        
-        // weighted sum
-        double z = 0.0;
-        
-        for(int i = 0; i < x.length; i++) {
-        	z += x[i] * w[i];
-        }
-        
-        z += b;
-        
-        // activation function
-        double a = z > 0 ? 1.0: 0.0;
-        
-        // print activation (output)
-        System.out.println(a);
-    }
- 
+//@formatter:on
+
+public class App {
+
+	static double neuron(double[] x, double[] w, double b) {
+		double z = 0.0;
+
+		for (int i = 0; i < x.length; i++) {
+			z += x[i] * w[i];
+		}
+
+		z += b;
+
+		return z > 0 ? 1.0 : 0.0;
+	}
+	
+	static double and(double x1, double x2) {
+		return neuron(new double[] {x1, x2}, new double[] {1, 1}, -1);
+	}
+
+	public static void main(String[] args) {
+		for(int i = 0; i < 4; i++) {
+			double x1 = i/2;
+			double x2 = i%2;
+			
+			double output = and(x1, x2);
+			
+			System.out.printf("%d%d\t%d\n", (int)x1, (int)x2, (int)output);
+		}
+	}
+
 }
