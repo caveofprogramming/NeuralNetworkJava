5978d6792bab065860c677a3853990588b896a54
John Purcell <john@caveofprogramming.com>
2021-10-16 15:03:51 +0200

    01120 OR, NOR, NAND

--- neuralnetwork/src/main/java/cave/neuralnetwork/App.java
+++ neuralnetwork/src/main/java/cave/neuralnetwork/App.java
@@ -24,19 +24,39 @@ public class App {
 
 		return z > 0 ? 1.0 : 0.0;
 	}
-	
+
 	static double and(double x1, double x2) {
-		return neuron(new double[] {x1, x2}, new double[] {1, 1}, -1);
+		return neuron(new double[] { x1, x2 }, new double[] { 1, 1 }, -1);
+	}
+
+	static double or(double x1, double x2) {
+		return neuron(new double[] { x1, x2 }, new double[] { 1, 1 }, 0);
+	}
+
+	static double xor(double x1, double x2) {
+		return neuron(new double[] { x1, x2 }, new double[] { 1, 1 }, -1);
+	}
+
+	static double nor(double x1, double x2) {
+		return neuron(new double[] { x1, x2 }, new double[] { -1, -1 }, 1);
+	}
+
+	static double nand(double x1, double x2) {
+		return neuron(new double[] { x1, x2 }, new double[] { -1, -1 }, 2);
+	}
+
+	static double xnor(double x1, double x2) {
+		return neuron(new double[] { x1, x2 }, new double[] { 1, 1 }, -1);
 	}
 
 	public static void main(String[] args) {
-		for(int i = 0; i < 4; i++) {
-			double x1 = i/2;
-			double x2 = i%2;
-			
-			double output = and(x1, x2);
-			
-			System.out.printf("%d%d\t%d\n", (int)x1, (int)x2, (int)output);
+		for (int i = 0; i < 4; i++) {
+			double x1 = i / 2;
+			double x2 = i % 2;
+
+			double output = nor(x1, x2);
+
+			System.out.printf("%d%d\t%d\n", (int) x1, (int) x2, (int) output);
 		}
 	}
 
