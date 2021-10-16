7b797f650ef74c9ad8527c42a06e97d69c91464f
John Purcell <john@caveofprogramming.com>
2021-10-16 12:47:16 +0200

    01070 Coding Perceptron

--- neuralnetwork/src/main/java/cave/neuralnetwork/App.java
+++ neuralnetwork/src/main/java/cave/neuralnetwork/App.java
@@ -8,6 +8,28 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	// inputs
+        double[] x = {0, 1};
+        
+        // weights
+        double[] w = {0.5, 0.5};
+        
+        // bias
+        double b = 0.5;
+        
+        // weighted sum
+        double z = 0.0;
+        
+        for(int i = 0; i < x.length; i++) {
+        	z += x[i] * w[i];
+        }
+        
+        z += b;
+        
+        // activation function
+        double a = z > 0 ? 1.0: 0.0;
+        
+        // print activation (output)
+        System.out.println(a);
     }
 }
