fab547f211a4ab515d7c0fa225e1a21653fc8c8a
John Purcell <john@caveofprogramming.com>
2021-10-21 15:58:37 +0200

    02060 Multiplying Matrices By a Value

--- neuralnetwork/src/test/java/matrix/MatrixTest.java
+++ neuralnetwork/src/test/java/matrix/MatrixTest.java
@@ -9,36 +9,48 @@ import cave.matrix.Matrix;
 class MatrixTest {
 
 	@Test
+	public void testMultiplyDouble() {
+		Matrix m = new Matrix(3, 4, i -> 0.5 * (i - 6));
+		
+		double x = 0.5;
+		
+		Matrix result = m.apply((index, value)->x * value);
+				
+		System.out.println(m);
+		System.out.println(result);
+	}
+
+	@Test
 	public void testToString() {
-		Matrix m = new Matrix(3, 4, i->i*2);
-		
+		Matrix m = new Matrix(3, 4, i -> i * 2);
+
 		String text = m.toString();
-		
+
 		double[] expected = new double[12];
-		
-		for(int i = 0; i < expected.length; i++) {
+
+		for (int i = 0; i < expected.length; i++) {
 			expected[i] = i * 2;
 		}
-		 
+
 		var rows = text.split("\n");
-		
+
 		assertTrue(rows.length == 3);
-		
+
 		int index = 0;
-		
-		for(var row: rows) {
+
+		for (var row : rows) {
 			var values = row.split("\\s+");
-			
-			for(var textValue: values) {
-				
-				if(textValue.length() == 0) {
+
+			for (var textValue : values) {
+
+				if (textValue.length() == 0) {
 					continue;
 				}
-				
+
 				var doubleValue = Double.valueOf(textValue);
-				
+
 				assertTrue(Math.abs(doubleValue - expected[index]) < 0.0001);
-				
+
 				index++;
 			}
 		}
