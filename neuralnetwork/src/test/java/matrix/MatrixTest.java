e329498f22908443bcb619c9657f9df144274f35
John Purcell <john@caveofprogramming.com>
2021-10-21 15:10:39 +0200

    02040 Testing the toString Method

--- neuralnetwork/src/test/java/matrix/MatrixTest.java
+++ neuralnetwork/src/test/java/matrix/MatrixTest.java
@@ -1,5 +1,7 @@
 package matrix;
 
+import static org.junit.Assert.assertTrue;
+
 import org.junit.jupiter.api.Test;
 
 import cave.matrix.Matrix;
@@ -10,7 +12,36 @@ class MatrixTest {
 	public void constructionTest() {
 		Matrix m = new Matrix(3, 4, i->i*2);
 		
-		System.out.println(m);
+		String text = m.toString();
+		
+		double[] expected = new double[12];
+		
+		for(int i = 0; i < expected.length; i++) {
+			expected[i] = i * 2;
+		}
+		 
+		var rows = text.split("\n");
+		
+		assertTrue(rows.length == 3);
+		
+		int index = 0;
+		
+		for(var row: rows) {
+			var values = row.split("\\s+");
+			
+			for(var textValue: values) {
+				
+				if(textValue.length() == 0) {
+					continue;
+				}
+				
+				var doubleValue = Double.valueOf(textValue);
+				
+				assertTrue(Math.abs(doubleValue - expected[index]) < 0.0001);
+				
+				index++;
+			}
+		}
 	}
 
 }
