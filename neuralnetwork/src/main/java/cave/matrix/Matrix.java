817f2492fd2823673ee2a9bc1e56669a99ef0193
John Purcell <john@caveofprogramming.com>
2021-10-21 14:02:58 +0200

    02030 Matrix toString Method

--- neuralnetwork/src/main/java/cave/matrix/Matrix.java
+++ neuralnetwork/src/main/java/cave/matrix/Matrix.java
@@ -2,6 +2,11 @@ package cave.matrix;
 
 public class Matrix {
 	
+	private static final String NUMBER_FORMAT = "%12.5f";
+	
+	private int rows;
+	private int cols;
+	
 	public interface Producer {
 		double produce(int index);
 	}
@@ -9,14 +14,36 @@ public class Matrix {
 	private double[] a;
 	
 	public Matrix(int rows, int cols) {
+		
+		this.rows = rows;
+		this.cols = cols;
+		
 		a = new double[rows * cols];
 	}
 	
 	public Matrix(int rows, int cols, Producer producer) {
-		a = new double[rows * cols];
+		this(rows, cols);
 		
 		for(int i = 0; i < a.length; i++) {
 			a[i] = producer.produce(i);
 		}
+	}
+	
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		
+		int index = 0;
+		
+		for(int row = 0; row < rows; row++) {
+			for(int col = 0; col < cols; col++) {
+				sb.append(String.format(NUMBER_FORMAT, a[index]));
+				
+				index++;
+			}
+			
+			sb.append("\n");
+		}
+		
+		return sb.toString();
 	}
 }
--- neuralnetwork/src/test/java/matrix/MatrixTest.java
+++ neuralnetwork/src/test/java/matrix/MatrixTest.java
@@ -9,6 +9,8 @@ class MatrixTest {
 	@Test
 	public void constructionTest() {
 		Matrix m = new Matrix(3, 4, i->i*2);
+		
+		System.out.println(m);
 	}
 
 }
