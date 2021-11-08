a4d4112aab97264381bce11a7de78b5562663953
John Purcell <john@caveofprogramming.com>
2021-11-08 12:53:15 +0100

    02220 Optimising Matrix Multiplication

--- neuralnetwork/src/main/java/cave/matrix/Matrix.java
+++ neuralnetwork/src/main/java/cave/matrix/Matrix.java
@@ -4,70 +4,69 @@ import java.util.Arrays;
 import java.util.Objects;
 
 public class Matrix {
-	
+
 	private static final String NUMBER_FORMAT = "%+12.5f";
 	private static final double TOLERANCE = 0.000001;
-	
+
 	private int rows;
 	private int cols;
-	
+
 	public interface Producer {
 		double produce(int index);
 	}
-	
+
 	public interface ValueProducer {
 		double produce(int index, double value);
 	}
-	
+
 	private double[] a;
-	
+
 	public Matrix(int rows, int cols) {
-		
+
 		this.rows = rows;
 		this.cols = cols;
-		
+
 		a = new double[rows * cols];
 	}
-	
+
 	public Matrix(int rows, int cols, Producer producer) {
 		this(rows, cols);
-		
-		for(int i = 0; i < a.length; i++) {
+
+		for (int i = 0; i < a.length; i++) {
 			a[i] = producer.produce(i);
 		}
 	}
-	
+
 	public Matrix apply(ValueProducer producer) {
 		Matrix result = new Matrix(rows, cols);
-		
-		for(int i = 0; i < a.length; i++) {
+
+		for (int i = 0; i < a.length; i++) {
 			result.a[i] = producer.produce(i, a[i]);
 		}
-		
+
 		return result;
 	}
-	
+
 	public Matrix multiply(Matrix m) {
 		Matrix result = new Matrix(rows, m.cols);
-		
-		assert cols == m.rows: "Cannot multiply; wrong number of rows vs cols";
-		
-		for(int row = 0; row < result.rows; row++) {
-			for(int col = 0; col < result.cols; col++) {
-			
-				for(int n = 0; n < cols; n++) {
+
+		assert cols == m.rows : "Cannot multiply; wrong number of rows vs cols";
+
+		for (int row = 0; row < result.rows; row++) {
+			for (int n = 0; n < cols; n++) {
+				for (int col = 0; col < result.cols; col++) {
 					result.a[row * result.cols + col] += a[row * cols + n] * m.a[col + n * m.cols];
-				} 
+				}
 			}
 		}
-	
+
 		return result;
 	}
-	
+
 	public double get(int index) {
 		return a[index];
 	}
-	
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -86,31 +85,31 @@ public class Matrix {
 		if (getClass() != obj.getClass())
 			return false;
 		Matrix other = (Matrix) obj;
-		
-		for(int i =0; i < a.length; i++) {
-			if(Math.abs(a[i] - other.a[i]) > TOLERANCE) {
+
+		for (int i = 0; i < a.length; i++) {
+			if (Math.abs(a[i] - other.a[i]) > TOLERANCE) {
 				return false;
 			}
 		}
-		 
+
 		return true;
 	}
 
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
-		
+
 		int index = 0;
-		
-		for(int row = 0; row < rows; row++) {
-			for(int col = 0; col < cols; col++) {
+
+		for (int row = 0; row < rows; row++) {
+			for (int col = 0; col < cols; col++) {
 				sb.append(String.format(NUMBER_FORMAT, a[index]));
-				
+
 				index++;
 			}
-			
+
 			sb.append("\n");
 		}
-		
+
 		return sb.toString();
 	}
 }
