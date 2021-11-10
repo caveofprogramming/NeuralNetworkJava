3e9be4ec6b7e82ed86b8db9cfd38a92bef926c28
John Purcell <john@caveofprogramming.com>
2021-11-10 14:04:47 +0100

    03040 Multiple Columns of Input

--- neuralnetwork/src/test/java/cave/neuralnetwork/NeuralNetTest.java
+++ neuralnetwork/src/test/java/cave/neuralnetwork/NeuralNetTest.java
@@ -1,5 +1,7 @@
 package cave.neuralnetwork;
 
+import static org.junit.Assert.assertTrue;
+
 import org.junit.jupiter.api.Test;
 
 import cave.matrix.Matrix;
@@ -7,18 +9,20 @@ import cave.matrix.Matrix;
 class NeuralNetTest {
 
 	@Test
-	void test() {
+	void testAddBias() {
+
+		Matrix input = new Matrix(3, 3, i -> (i + 1));
+		Matrix weights = new Matrix(3, 3, i -> (i + 1));
+		Matrix biases = new Matrix(3, 1, i -> (i + 1));
+
+		Matrix result = weights.multiply(input).modify((row, col, value) -> value + biases.get(row));
+
+		double[] expectedValues = { +31.00000, +37.00000, +43.00000, +68.00000, +83.00000, +98.00000, +105.00000,
+				+129.00000, +153.00000 };
 		
-		Matrix input = new Matrix(3, 1, i->(i+1));
-		Matrix weights = new Matrix(3, 3, i->(i+1));
-		Matrix biases = new Matrix(3, 1, i->(i+1));
+		Matrix expected = new Matrix(3, 3, i->expectedValues[i]);
 		
-		Matrix result = weights.multiply(input).modify((row, col, value)->value + biases.get(row));
-		
-		System.out.println(input);
-		System.out.println(weights);
-		System.out.println(biases);
-		System.out.println(result);
+		assertTrue(expected.equals(result));
 	}
 
 }
