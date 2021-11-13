7f0ba7af4b79583a80eb24b07a149e711d42cead
John Purcell <john@caveofprogramming.com>
2021-11-13 13:38:37 +0100

    03070 forEach

--- neuralnetwork/src/main/java/cave/matrix/Matrix.java
+++ neuralnetwork/src/main/java/cave/matrix/Matrix.java
@@ -15,10 +15,18 @@ public class Matrix {
 		double produce(int index);
 	}
 
-	public interface ValueProducer {
+	public interface IndexValueProducer {
 		double produce(int index, double value);
 	}
-	
+
+	public interface ValueProducer {
+		double produce(double value);
+	}
+
+	public interface IndexValueConsumer {
+		void consume(int index, double value);
+	}
+
 	public interface RowColProducer {
 		double produce(int row, int col, double value);
 	}
@@ -41,7 +49,7 @@ public class Matrix {
 		}
 	}
 
-	public Matrix apply(ValueProducer producer) {
+	public Matrix apply(IndexValueProducer producer) {
 		Matrix result = new Matrix(rows, cols);
 
 		for (int i = 0; i < a.length; i++) {
@@ -50,21 +58,37 @@ public class Matrix {
 
 		return result;
 	}
-	
+
 	public Matrix modify(RowColProducer producer) {
-		
+
 		int index = 0;
-		
-		for(int row = 0; row < rows; ++row) {
-			for(int col = 0; col < cols; ++col) {
-				
+
+		for (int row = 0; row < rows; ++row) {
+			for (int col = 0; col < cols; ++col) {
+
 				a[index] = producer.produce(row, col, a[index]);
-				
+
 				++index;
 			}
 		}
-		
+
 		return this;
+	}
+
+	public Matrix modify(ValueProducer producer) {
+
+		for (int i = 0; i < a.length; ++i) {
+
+			a[i] = producer.produce(a[i]);
+		}
+
+		return this;
+	}
+
+	public void forEach(IndexValueConsumer consumer) {
+		for (int i = 0; i < a.length; ++i) {
+			consumer.consume(i, a[i]);
+		}
 	}
 
 	public Matrix multiply(Matrix m) {
