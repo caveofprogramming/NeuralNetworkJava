30ff61ee3785584d5352f05a0894818d3dc1f685
John Purcell <john@caveofprogramming.com>
2021-11-16 12:31:12 +0100

    03170 Configuring Dense Layers

--- neuralnetwork/src/main/java/cave/neuralnetwork/Engine.java
+++ neuralnetwork/src/main/java/cave/neuralnetwork/Engine.java
@@ -1,22 +1,40 @@
 package cave.neuralnetwork;
 
 import java.util.LinkedList;
+import java.util.Random;
+
+import cave.matrix.Matrix;
 
 public class Engine {
 	private LinkedList<Transform> transforms = new LinkedList<>();
-	
-	public void add(Transform transform) {
+	private LinkedList<Matrix> weights = new LinkedList<>();
+	private LinkedList<Matrix> biases = new LinkedList<>();
+
+	public void add(Transform transform, double... params) {
+		
+		Random random = new Random();
+		
+		if(transform == Transform.DENSE) {
+			int numberNeurons = (int)params[0];
+			int weightsPerNeuron = weights.size() == 0 ? (int)params[1]: weights.getLast().getRows();
+			
+			Matrix weight = new Matrix(numberNeurons, weightsPerNeuron, i->random.nextGaussian());
+			Matrix bias = new Matrix(numberNeurons, 1, i->random.nextGaussian());
+			
+			weights.add(weight);
+			biases.add(bias);
+		}
 		transforms.add(transform);
 	}
-	
+
 	@Override
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
-		
-		for(var t: transforms) {
+
+		for (var t : transforms) {
 			sb.append(t).append("\n");
 		}
-		
+
 		return sb.toString();
 	}
 }
