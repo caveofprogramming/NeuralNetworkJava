0df3191fd892332cde64bb41674c0a9d93badabe
John Purcell <john@caveofprogramming.com>
2021-12-03 12:20:23 +0100

    05040 A Calculus Class

---
+++ neuralnetwork/src/main/java/cave/calculus/Calculus.java
@@ -0,0 +1,27 @@
+package cave.calculus;
+
+import java.util.function.DoubleFunction;
+
+public class Calculus {
+	
+	private static final double INC = 0.00001;
+	
+	public static double func1(double x) {
+		return 3.7 * x + 5.3;
+	}
+	
+	public static double func2(double x) {
+		return x * x;
+	}
+	
+	public static double differentiate(DoubleFunction<Double> func, double x) {
+		
+		
+		
+		return 0;
+	}
+
+	public static void main(String[] args) {
+
+	}
+}
