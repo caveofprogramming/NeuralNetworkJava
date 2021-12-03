c8693a0345bcb102e0aa9056a94d2a0a808d639e
John Purcell <john@caveofprogramming.com>
2021-12-03 12:35:35 +0100

    05050 Implementing Differentiation

--- neuralnetwork/src/main/java/cave/calculus/Calculus.java
+++ neuralnetwork/src/main/java/cave/calculus/Calculus.java
@@ -11,17 +11,24 @@ public class Calculus {
 	}
 	
 	public static double func2(double x) {
-		return x * x;
+		return x * x - 3.23;
 	}
 	
 	public static double differentiate(DoubleFunction<Double> func, double x) {
 		
+		double output1 = func.apply(x);
+		double output2 = func.apply(x + INC);
 		
 		
-		return 0;
+		return (output2 - output1)/INC;
 	}
 
 	public static void main(String[] args) {
-
+		
+		for(double x = -2; x < 2; x += 0.1) {
+			
+			double gradient = differentiate(Calculus::func2, x);
+			System.out.printf("%.2f\t%.2f\n", x, gradient);
+		}
 	}
 }
