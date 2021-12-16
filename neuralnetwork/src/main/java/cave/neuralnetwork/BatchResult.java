dc61291fddd794760e0f04290409e4d9cdb9e9c5
John Purcell <john@caveofprogramming.com>
2021-12-16 12:57:00 +0100

    06060 Storing Errors

--- neuralnetwork/src/main/java/cave/neuralnetwork/BatchResult.java
+++ neuralnetwork/src/main/java/cave/neuralnetwork/BatchResult.java
@@ -6,6 +6,8 @@ import cave.matrix.Matrix;
 
 public class BatchResult {
 	private LinkedList<Matrix> io = new LinkedList<>();
+	private LinkedList<Matrix> weightErrors = new LinkedList<>();
+	private Matrix inputError;
 	
 	public LinkedList<Matrix> getIo() {
 		return io;
@@ -14,4 +16,22 @@ public class BatchResult {
 	public void addIo(Matrix m) {
 		io.add(m);
 	}
+
+	public LinkedList<Matrix> getWeightErrors() {
+		return weightErrors;
+	}
+
+	public void addWeightError(Matrix weightError) {
+		weightErrors.addFirst(weightError);
+	}
+
+	public Matrix getInputError() {
+		return inputError;
+	}
+
+	public void setInputError(Matrix inputError) {
+		this.inputError = inputError;
+	}
+	
+	
 }
