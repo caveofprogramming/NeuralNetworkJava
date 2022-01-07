841d57d6099dcfe69cef55aeef0a4d186c1e689b
John Purcell <john@caveofprogramming.com>
2022-01-07 16:54:45 +0100

    08120 The Fit Method

--- neuralnetwork/src/main/java/cave/neuralnetwork/NeuralNetwork.java
+++ neuralnetwork/src/main/java/cave/neuralnetwork/NeuralNetwork.java
@@ -1,7 +1,14 @@
 package cave.neuralnetwork;
 
+import cave.neuralnetwork.loader.Loader;
+
 public class NeuralNetwork {
 	private Engine engine;
+	
+	private int epochs;
+	private double learningRate;
+	private double initialLearningRate = 0.01;
+	private double finalLearningRate = 0;
 	
 	public NeuralNetwork() {
 		engine = new Engine();
@@ -9,6 +16,38 @@ public class NeuralNetwork {
 	
 	public void add(Transform transform, double... params) {
 		engine.add(transform, params);
+	}
+	
+	public void setLearningRates(double initialLearningRate, double finalLearningRate) {
+		this.initialLearningRate = initialLearningRate;
+		this.finalLearningRate = finalLearningRate;
+	}
+	
+	public void setEpochs(int epochs) {
+		this.epochs = epochs;
+	}
+	
+	public void fit(Loader trainLoader, Loader evalLoader) {
+		
+		learningRate = initialLearningRate;
+		
+		for(int epoch = 0; epoch < epochs; epoch++) {
+			
+			System.out.printf("Epoch %3d ", epoch);
+			
+			runEpoch(trainLoader, true);
+			
+			if(evalLoader != null) {
+				runEpoch(evalLoader, false);
+			}
+			
+			learningRate -= (initialLearningRate - finalLearningRate)/epochs;
+		}
+	}
+
+	private void runEpoch(Loader loader, boolean trainingMode) {
+		
+		
 	}
 
 	@Override
