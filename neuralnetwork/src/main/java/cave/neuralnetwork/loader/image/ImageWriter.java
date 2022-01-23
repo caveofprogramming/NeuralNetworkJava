ed789bbb94c468b37cf88037492ac202d070bcc3
John Purcell <john@caveofprogramming.com>
2022-01-23 03:52:30 +0100

    09140 Writing Images

--- neuralnetwork/src/main/java/cave/neuralnetwork/loader/image/ImageWriter.java
+++ neuralnetwork/src/main/java/cave/neuralnetwork/loader/image/ImageWriter.java
@@ -3,9 +3,11 @@ package cave.neuralnetwork.loader.image;
 import java.io.File;
 import java.io.IOException;
 
+import javax.imageio.ImageIO;
+
+import java.awt.image.BufferedImage;
+
 import cave.neuralnetwork.loader.BatchData;
-import cave.neuralnetwork.loader.Loader;
-import cave.neuralnetwork.loader.MetaData;
 
 public class ImageWriter {
 	public static void main(String[] args) {
@@ -37,9 +39,11 @@ public class ImageWriter {
 		final String trainLabels = String.format("%s%s%s", directory, File.separator, "train-labels-idx1-ubyte");
 		final String testImages = String.format("%s%s%s", directory, File.separator, "t10k-images-idx3-ubyte");
 		final String testLabels = String.format("%s%s%s", directory, File.separator, "t10k-labels-idx1-ubyte");
+		
+		int batchSize = 900;
 
-		ImageLoader trainLoader = new ImageLoader(trainImages, trainLabels, 32);
-		ImageLoader testLoader = new ImageLoader(testImages, testLabels, 32);
+		ImageLoader trainLoader = new ImageLoader(trainImages, trainLabels, batchSize);
+		ImageLoader testLoader = new ImageLoader(testImages, testLabels, batchSize);
 		
 		ImageLoader loader = testLoader;
 
@@ -47,6 +51,18 @@ public class ImageWriter {
 		
 		for (int i = 0; i < metaData.getNumberBatches(); i++) {
 			BatchData batchData = testLoader.readBatch();
+			
+			String montagePath = String.format("montage%d.jpg", i);
+			System.out.println("Writing " + montagePath);
+			
+			var montage = new BufferedImage(900, 900, BufferedImage.TYPE_BYTE_GRAY);
+			
+			try {
+				ImageIO.write(montage, "jpg", new File(montagePath));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 
 		loader.close();
